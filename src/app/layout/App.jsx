3a79f45c128349084cc5ac9b1f51ce67185e69eb
Rom Vic <technoyguy@gmail.com>
2018-09-16 12:50:26 -0700

    initial comit

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div >
+        <h1>Learn Tagalog App</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
+
